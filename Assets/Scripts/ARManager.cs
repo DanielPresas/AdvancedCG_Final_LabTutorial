1891a37a010f475b87ad730769c323dde23554cc
Synral <44206227+Synral@users.noreply.github.com>
2022-04-19 23:28:38 -0400

    update tutorial

--- Assets/Scripts/ARManager.cs
+++ Assets/Scripts/ARManager.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-public class ARManager : MonoBehaviour
-{
+public class ARManager : MonoBehaviour {
     [SerializeField] PlaceDevice device;
-    [SerializeField] GameObject[] prefabs;
-    private void Start() {
+    [SerializeField] GameObject[] stepsUI;
+    int _index = 0;
+    public void NextButton() {
+        foreach(var obj in device.demoObjs)
+            obj.SetActive(false);
 
+        if(_index < stepsUI.Length - 1) {
+            stepsUI[_index].SetActive(false);
+            _index++;
+            stepsUI[_index].SetActive(true);
+        }
+
+        if(_index == 2)
+            device.demoObjs[0].SetActive(true);
+        else if(_index == 5)
+            device.demoObjs[1].SetActive(true);
     }
+    public void BackButton() {
+        foreach(var obj in device.demoObjs)
+            obj.SetActive(false);
 
-    private void Update() {
+        if(_index > 0) {
+            stepsUI[_index].SetActive(false);
+            _index--;
+            stepsUI[_index].SetActive(true);
+        }
 
+        if(_index == 2)
+            device.demoObjs[0].SetActive(true);
+        else if(_index == 5)
+            device.demoObjs[1].SetActive(true);
     }
 }
--- Assets/Scripts/PlaceDevice.cs
+++ Assets/Scripts/PlaceDevice.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Experimental.XR;
 using UnityEngine.XR.ARFoundation;
@@ -10,9 +11,14 @@ public class PlaceDevice : MonoBehaviour {
     [SerializeField] ARSessionOrigin arOrigin;
     [SerializeField] ARRaycastManager aRRaycastManager;
     [SerializeField] GameObject placementIndicator;
-    [SerializeField] public GameObject prefab;
+    [SerializeField] GameObject setupUI;
+    [SerializeField] GameObject firstStepUI;
+    [SerializeField] GameObject bottomUI;
+    [SerializeField] GameObject[] prefabs;
+    public List<GameObject> demoObjs;
     Pose _placementPose;
     bool _placementPoseIsValid = false;
+    bool _placedObj = false;
     // Start is called before the first frame update
     void Start() {
 
@@ -23,14 +29,27 @@ public class PlaceDevice : MonoBehaviour {
         UpdatePlacementPose();
         UpdatePlacementIndicator();
 
-        if (_placementPoseIsValid && Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        // Only one instance can be placed per session. Enables the buttons and starts the tutorial while disabling setup/placement indicator.
+        if(_placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && !_placedObj) {
+            _placedObj = true;
             PlaceObject();
+            placementIndicator.SetActive(false);
+            setupUI.SetActive(false);
+            bottomUI.SetActive(true);
+            firstStepUI.SetActive(true);
+        }
     }
 
+    // Places and hides the volumetric captures for later use based on ray-traced location from camera for AR
     void PlaceObject() {
-        Instantiate(prefab, _placementPose.position, _placementPose.rotation);
+        foreach(var obj in prefabs) {
+            var temp = Instantiate(obj, _placementPose.position, _placementPose.rotation);
+            temp.SetActive(false);
+            demoObjs.Add(temp);
+        }
     }
 
+    // Updates the placement indicator's position/rotation, and disables it if a spot is invalid for placement.
     private void UpdatePlacementIndicator() {
         if(_placementPoseIsValid) {
             placementIndicator.SetActive(true);
@@ -40,7 +59,10 @@ public class PlaceDevice : MonoBehaviour {
             placementIndicator.SetActive(false);
     }
 
+    // Raycasts and checks if a plane can be projected; if it can, it updates the position/rotation of the placement pose.
     void UpdatePlacementPose() {
+        if (Camera.current == null)
+            return;
         var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
         var hits = new List<ARRaycastHit>();
         aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
